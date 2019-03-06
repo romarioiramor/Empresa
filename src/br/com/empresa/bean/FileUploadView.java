e931cceb4d08d98b317aa19bc5a60daa9dcda28d
romario <romario.adsistemas@gmail.com>
2019-03-06 10:39:13 -0300

    Projeto atualizado

---
+++ src/br/com/empresa/bean/FileUploadView.java
@@ -0,0 +1,34 @@
+package br.com.empresa.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+@ManagedBean
+public class FileUploadView {
+     
+    private UploadedFile file;
+ 
+    public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+     
+    public void upload() {
+        if(file != null) {
+            FacesMessage message = new FacesMessage("Enviado com sucesso", file.getFileName() + " e carregado.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+     
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage msg = new FacesMessage("Enviado com sucesso", event.getFile().getFileName() + " e carregado.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+}
